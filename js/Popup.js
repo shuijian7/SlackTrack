2fb7e70a60f51640f007fe773bf47d37f85846c7
Will Lin <plin6@uoregon.edu>
2019-02-27 19:09:46 -0800

    UI updated

--- js/Popup.js
+++ js/Popup.js
@@ -17,9 +17,31 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    document.body.style.overflow = 'hidden'
   }
+}
+
+
+
+
+window.onscroll = function() {scrollFunction()};
+						
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn2").style.display = "block";
+    } else {
+    document.getElementById("myBtn2").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
