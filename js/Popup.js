aafa8b77cf0f2c7eb1089c145acd87fbf38af18e
Will Lin <plin6@uoregon.edu>
2019-03-04 19:36:25 -0800

    Monday update

--- js/Popup.js
+++ js/Popup.js
@@ -1,29 +1,31 @@
+
 // Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var btn3 = document.getElementById("myBtn3");
+// var modal = document.getElementById('myModal');
 
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-  document.getElementById('myMajor').innerHTML= "";
-  document.getElementById('myMajor').innerHTML = "<option value='NNN' disabled selected>Select  professor</option>"
-  + "<option value='Hank'>Hank Childs</option>"
-  + "<option value='Lei'>Lei Jiao</option>"
-  + "<option value='Anthony'>Anthony Hornof</option>"
-  + "<option value='Joe'>Joe Sventek</option>";
-  document.body.style.overflow = 'hidden';
-
-}
+// // Get the button that opens the modal
+// var Btn122 = document.getElementById("Btn122");
+// var Btn210 = document.getElementById("Btn210");
 
 
-btn3.onclick = function() {
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// Btn122.onclick = function() {
+//   modal.style.display = "block";
+//   document.getElementById('myMajor').innerHTML= "";
+//   document.getElementById('myMajor').innerHTML = "<option value='NNN' disabled selected>Select  professor</option>"
+//   + "<option value='Hank'>Hank Childs</option>"
+//   + "<option value='Lei'>Lei Jiao</option>"
+//   + "<option value='Anthony'>Anthony Hornof</option>"
+//   + "<option value='Joe'>Joe Sventek</option>";
+//   document.body.style.overflow = 'hidden';
+
+// }
+
+
+Btn210.onclick = function() {
   modal.style.display = "block";
   document.getElementById('myMajor').innerHTML= "";
   document.getElementById('myMajor').innerHTML = "<option value='Hank'>WHAT? Hank Childs</option>";
