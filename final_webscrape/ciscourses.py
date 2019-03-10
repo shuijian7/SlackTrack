72bfb9386a3d513c6c5c8fb787cfb7465dd4ac39
Claire Phillips <claireannphillips15@gmail.com>
2019-03-09 16:00:01 -0800

    final webscraper

---
+++ final_webscrape/ciscourses.py
@@ -0,0 +1,99 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import pickle
+
+
+my_url = 'http://catalog.uoregon.edu/arts_sciences/computerandinfoscience/#courseinventory'
+
+#opening up conneciton, grabbing the page
+
+data = []
+
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+page_soup = soup(page_html, "html.parser")
+
+#grabs containers
+containers = page_soup.findAll("div",{"class":"courseblock"})
+
+#for csv file 
+filename = "cis_courses.txt"
+f = open(filename, "w")
+
+#headers = "Major" 
+#f.write(headers)
+
+l1 = []
+ex = []
+redo = []
+plz = []
+
+for container in containers:
+    #print(container)
+    #newtext = container.replace("&nbsp", "")
+    #print(newtext)
+    final = container.p.text
+    final = final.strip('\xa0').split(" ")    
+    data.append(final)
+    #print(data[0])
+    ####print(final)
+    ex.append(final)
+    for line in final:
+        #course = line[0]
+        #print(course)
+        #num = line[1]
+        #print("inside for loop")
+        l1.extend(line.strip('').split('\xa0'))
+        #l1.append(course.strip('').split('\xa0'))
+        #l1.append(num.strip('').split('\xa0'))
+        #redo.append(ep[1:1])
+    #line = str(l1)
+    #f.write(line)
+    first = final[0]
+    #print(first)
+    data.append(final)
+   
+    final.append(first)
+ 
+    #print(final)
+    #print(final)
+    #print(final)
+    #data.append(final)
+#f.write(l1)
+    #f.write(data[container]) 
+    #print(final)
+#pickle.dump(l1, fileObject, protocol=2)
+#for line in data:
+    #l1.extend(line.strip('').split('\xa0'))
+#print(l1)
+def classInfo():
+    #once get it ust write it an call this function after the for toop in ex
+    for info in ex:
+        info = info[3:7]
+        print(info)
+        
+#print(ex)
+for toop in ex:
+    toop = toop[:1]
+    #toop = toop[:7]
+    #print(toop) 
+    redo.extend(toop)
+    #str(toop)
+    #toop.strip('\xa0').split(" ")
+    ###redo.extend(toop.strip('').split('\xa0'))
+    #print(toop)
+for thing in redo:
+    plz.append(thing.strip('').split('\xa0'))
+
+for item in plz:
+    f.write("%s\n" % item)   
+print(plz)
+#print(ex)
+f.close()
+#classInfo()
+#print(ex)
+
+
+
---
+++ final_webscrape/combine_philo.py
@@ -0,0 +1,83 @@
+
+filename = "final_philo_courses.txt"
+x = open(filename, "w")
+
+hold_class = []
+hold_info = []
+final = []
+
+f = open("philo_courses.txt", "r")
+for item in f:
+    hold_class.append(item.strip('"').strip('\n'))
+
+f2 = open("philo_info.txt", "r")
+for item2 in f2:
+    hold_info.append(item2.strip('\n'))
+
+for i in range(len(hold_class)):
+    first = hold_class[i]
+    second = hold_info[i]
+    final.append(first + second)
+
+#####course = f.readline()
+#print(course)
+##for items in course:
+    ##hold_class.append(items.rstrip('\n'))
+#print(f2.readline())
+
+'''
+with open("courses.txt", "r") as f:
+    hold_class.extend(f)
+    for line in f:
+        print(line)
+        mylist = line.strip('\n')
+        #mylist = list(f)
+        #mylist.rstrip('\n')
+        #hold_class.extend(line)
+'''        
+
+  
+
+'''
+for lines in f2:
+    f2.readline()
+    first = lines[0]
+    second = lines[1]
+    #print(f2.read())
+    #print(first)
+    #print(second)
+#for thing in inform:
+    #hold.extend(thing)
+
+f = open("courses.txt", "r")
+#print(f.readline())
+
+test = []
+final_info = []
+for line in inform:
+    first = line[0] 
+    second = line[1]
+    #combine = first + second
+    #print(combine) 
+    #course = f.readline()
+    #info = f2.readline()
+    #test.append(course)
+    #test.extend(info)
+    x.write(course)
+    x.write(info)
+    #print(course)
+    #print(info)
+'''
+#print(len(hold_info))
+#print(len(hold_class))
+print(final)
+#print(final[0])
+#print(inform)
+
+for thing in final:
+    x.write("%s\n" % thing)
+
+x.close()
+
+
+#for loop over info and then make object 0 the course and object 1 the credits and combine them and then combine classes with it
