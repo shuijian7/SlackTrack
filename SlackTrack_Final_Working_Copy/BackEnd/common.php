235e051c62a5a2f3293946b018b8e4e16790a34d
Tyler Millan <44144628+tylermillan@users.noreply.github.com>
2019-03-12 09:49:22 -0700

    Update common.php

--- SlackTrack_Final_Working_Copy/BackEnd/common.php
+++ SlackTrack_Final_Working_Copy/BackEnd/common.php
@@ -1,6 +1,8 @@
 <?php
 /**
- Derived from https://ilovephp.jondh.me.uk/en/tutorial/make-your-own-blog, called by php database functions in index.php. Is used around html variables in php functions to maintain html properties which allows for more consistent handling of variables. Written by Tyler Millan.
+ Derived from https://ilovephp.jondh.me.uk/en/tutorial/make-your-own-blog, called by the major pages back end files housed
+ in the same folder. Is used around html variables in php functions to maintain html properties which allows for more 
+ consistent handling of variables. Written by Tyler Millan. Last Updated 05/12/2019.
  *
  */
 function escape($html) {
