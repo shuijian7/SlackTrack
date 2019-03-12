dd782f5b2fb1cfe363591557da5a61561e61f88e
Tyler Millan <44144628+tylermillan@users.noreply.github.com>
2019-03-12 09:53:03 -0700

    Update config.php

--- SlackTrack_Final_Working_Copy/BackEnd/config.php
+++ SlackTrack_Final_Working_Copy/BackEnd/config.php
@@ -1,7 +1,11 @@
 <?php
 
 /**
- * This is a helper file that houses the variables for connecting to the database. When called upon by the index.php file, it passes these variables. Allows for greater information hiding for index file. Written by Tyler Millan.
+ * This is a helper file that houses the variables for connecting to the database. 
+ When called upon by the the major pages back end files, it passes variables to connect to the database. 
+ Allows for greater information hiding for back end files. Written by Tyler Millan. Last Updated 05/12/2019
+ 
+ The database we are connecting to in this is hosted on the University of Oregon CIS department's ix-dev server. 
  */
 
 $host       = "ix.cs.uoregon.edu";
